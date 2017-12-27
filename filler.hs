9b8f529076aaf23eaa103bb468e00ad1ec92b232
josed92 <jose_daniel_d@hotmail.com>
2017-12-27 00:48:07 -0400

    now using Control.Monad.Trans.Random for simplicity

--- filler.hs
+++ filler.hs
@@ -18,7 +18,6 @@ import qualified Data.ByteString as BS
 import Data.ByteString.Internal
 import Control.Monad
 import Data.Word
-import Control.Monad.Random
 import qualified Data.Vector.Storable as VS
 import qualified Data.Vector.Storable.Mutable as VSM
 import Control.Concurrent.Async
@@ -26,59 +25,51 @@ import Control.DeepSeq
 import Foreign.ForeignPtr
 import Control.Monad.ST
 import Control.Monad.Primitive
+import Control.Monad.Random.Class
+import Control.Monad.Trans.Random.Strict
 
 hi = 10000000
 lo = 1000000
 tam = 10000000
 
-newChar32 :: RandomGen g => g -> ST s (Word32,g)
-newChar32 = return . random
-
-newChar64 :: RandomGen g => g -> ST s (Word64,g)
-newChar64 = return . random
-
 #ifdef x86_64_HOST_ARCH
-newChar :: RandomGen g => g -> ST s (Word64,g)
-newChar = newChar64
+newChar :: RandomGen g => RandST g s Word64
+newChar = getRandom
 factor = 8
 type WordType = Word64
 #else
-newChar :: RandomGen g => g -> ST s (Word32,g)
-newChar = newChar32
+newChar :: RandomGen g => RandST g s Word32
+newChar = getRandom
 factor = 4
 type WordType = Word32
 #endif
 
-
-files = map ((++ ".random") . show) [0..]
+files = map ((++ ".random") . show) [0..5]
 
 type RandST g s a = RandT g (ST s) a
 
-modifyM :: (RandomGen g, VSM.Storable a) => g -> VSM.MVector s a -> (g -> ST s (a,g)) -> ST s g
-modifyM gen vect m = modifyMAux gen max 0 vect m
-  where max = VSM.length vect
+modifyM :: (RandomGen g, VSM.Storable a) => VSM.MVector s a -> RandST g s a -> RandST g s ()
+modifyM vect m = modifyMAux 0
+  where
+    max = VSM.length vect
+    modifyMAux current
+      | max == current = return ()
+      | otherwise = m >>= VSM.unsafeWrite vect current >> modifyMAux (current+1)
 
-modifyMAux :: (RandomGen g, VSM.Storable a) => g -> Int -> Int -> VSM.MVector s a -> (g -> ST s (a,g)) -> ST s g 
-modifyMAux gen max current vect m
-  | max == current = return gen
-  | otherwise = do
-    (a,newGen) <- m gen
-    a `seq` VSM.unsafeWrite vect current a
-    modifyMAux newGen max (current+1) vect m
-
-randomizeVect :: RandomGen g => g -> VSM.MVector s WordType -> ST s g
-randomizeVect gen vect = modifyM gen vect newChar
+randomizeVect :: RandomGen g => VSM.MVector s WordType -> g -> ST s g
+randomizeVect vect = fmap snd . runRandT (modifyM vect newChar)
 
 makeVect :: RandomGen g => VS.Vector WordType -> g -> IO (VS.Vector WordType,g)
 makeVect vect gen = return $ runST $ do
   vectM <- VS.unsafeThaw vect
-  g <- randomizeVect gen vectM
+  g <- randomizeVect vectM gen
   newvect <- VS.unsafeFreeze vectM
   return (newvect,g)
 
 vect2Bs vect = fromForeignPtr (castForeignPtr ptr) 0 (len*factor)
   where (ptr,len) = VS.unsafeToForeignPtr0 vect
 
+myio _ [] _ = return []
 myio gen (f:files) vect = do
   (newvect,newGen) <- makeVect vect gen
   BS.writeFile f $ vect2Bs newvect
@@ -89,9 +80,3 @@ main = do
   gen <- TF.newTFGen
   let newVect = VS.replicate tam 0
   myio gen files newVect
-
-
-
-
-
-
--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 x86_64: filler.hs zerofiller.hs
-	ghc filler.hs -cpp
-	ghc zerofiller.hs -cpp
+	ghc filler.hs -cpp -rtsopts
+	ghc zerofiller.hs -cpp -rtsopts
 
 i386: filler.hs zerofiller.hs
 	ghc filler.hs -cpp
